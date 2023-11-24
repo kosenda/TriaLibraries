cd1c0dbe9284b649b65058ded86f1c331d5fc9cb
kosenda <kkkk0630@ezweb.ne.jp>
2023-11-24 18:43:31 +0900

    fix: remove unused import

--- app/src/main/java/ksnd/trialibraries/FirstScreen.kt
+++ app/src/main/java/ksnd/trialibraries/FirstScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
